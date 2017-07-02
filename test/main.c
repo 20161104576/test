3ec6ae95c413388faf2524c307286f68370b6518
20161104576 <a20161104576@admindeimac-33.local>
2017-07-02 19:38:27 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by fanjiahui on 17/7/2.
+//  Copyright © 2017年 fanjiahui. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
